8192bf3ae44de4f5a4b9956bc6a88a8dea927289
ccairoo <marigondon.ccarolinedr@gmail.com>
2025-11-18 17:01:18 +0800

    authentication
    login register web.php authcontroller

---
+++ CCDM_PORTFOLIO/app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    #same sa web.php dapat
+    function showRegister() {
+        return view('authentication.register');
+    }
+
+    function showLogin() {
+        return view('authentication.login');
+    }
+
+    function performLogin() {
+
+    }
+
+    function performRegister() {
+
+    }
+}
